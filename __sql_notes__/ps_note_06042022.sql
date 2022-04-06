d79043be00dd5bb66a90af50a6d58b54fe064e57
alee0510 <ali.muksin0510@gmail.com>
2022-04-06 20:38:46 +0700

    add new sql notes

--- __sql_notes__/ps_note_06042022.sql
+++ __sql_notes__/ps_note_06042022.sql
@@ -3,5 +3,36 @@ SHOW TABLES;
 
 -- PRACTICE
 -- 1.1 GET data employee(name) plus total customer
+SELECT em.employeeNumber,
+CONCAT( em.firstName, ' ',em.lastName) employeeName, 
+COUNT(cs.customerNumber) total_customer
+FROM employees AS em
+LEFT JOIN customers AS cs ON em.employeeNumber = cs.salesRepEmployeeNumber
+GROUP BY em.employeeNumber;
+
 -- 1.2 GET data employee(name) total customer, average credit limit, total credit limit
+SELECT em.employeeNumber,
+CONCAT( em.firstName, ' ',em.lastName) employeeName, 
+COUNT(cs.customerNumber) total_customer,
+AVG(cs.creditLimit) avg_credit,
+SUM(cs.creditLimit) total_credit
+FROM employees AS em
+LEFT JOIN customers AS cs ON em.employeeNumber = cs.salesRepEmployeeNumber
+GROUP BY em.employeeNumber;
+
 -- 1.3 GET employe name with highest total customer for each country / city
+SELECT employeeNumber, employeeName, country,
+MAX(total_customer) AS max_total_customer
+FROM (
+	SELECT em.employeeNumber, CONCAT( em.firstName, ' ',em.lastName) employeeName, 
+	COUNT(cs.customerNumber) total_customer,
+	cs.country
+	FROM employees AS em
+	JOIN customers AS cs ON em.employeeNumber = cs.salesRepEmployeeNumber
+	GROUP BY em.employeeNumber
+) AS result
+GROUP BY country;
+
+-- 1.3 GET employee data with 0 customer
+-- 1.4 GET employee data, job title, head/boss info, plus head/boss job title, order by highest total customer
+-- 1.5 ...1.4 plus office city info for each employee
