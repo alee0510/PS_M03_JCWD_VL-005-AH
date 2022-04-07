d8c58ace80d2e5300fca3c4f718ba10b6aa86d90
alee0510 <ali.muksin0510@gmail.com>
2022-04-07 19:19:31 +0700

    add new sql notes

---
+++ __sql_notes__/ps_notes_07042022.sql
@@ -0,0 +1,7 @@
+USE classicmodels;
+
+-- PRACTICES
+-- GET customers, its order (qty, status, product name) and payment info
+-- GET top 10 data employee with highest total credit limit from customers
+-- GET top 10 best selling product
+-- GET top 10 customers with highest buy
